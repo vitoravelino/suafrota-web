c77b64dddcb7b0c26a92f191792a6071c0cadd98
Vítor Avelino <contact@vitoravelino.me>
2016-08-23 19:42:03 -0300

    fixed hidden customer menu for non admins

--- src/modules/dashboard/components/header.vue
+++ src/modules/dashboard/components/header.vue
@@ -18,7 +18,7 @@
       <!-- Navbar Right Menu -->
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-          <customer-menu></customer-menu v-if="$auth.is('admin')">
+          <customer-menu v-if="$auth.is('admin')"></customer-menu>
           <user-account-menu></user-account-menu>
         </ul>
       </div>
