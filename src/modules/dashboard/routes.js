79c74052973582c4bca7de81d4158daa11f82d4f
Vítor Avelino <contact@vitoravelino.me>
2016-08-23 19:42:28 -0300

    used alias when importing routes

--- src/modules/dashboard/routes.js
+++ src/modules/dashboard/routes.js
@@ -1,11 +1,11 @@
 import Layout from './views/layout';
 
-import UsersRoutes from '../users/routes';
-import VehiclesRoutes from '../vehicles/routes';
-import EquipmentsRoutes from '../equipments/routes';
-import CustomersRoutes from '../customers/routes';
-import PermissionsRoutes from '../permissions/routes';
-import VehicleGroupsRoutes from '../vehicle_groups/routes';
+import UsersRoutes from 'modules/users/routes';
+import VehiclesRoutes from 'modules/vehicles/routes';
+import EquipmentsRoutes from 'modules/equipments/routes';
+import CustomersRoutes from 'modules/customers/routes';
+import PermissionsRoutes from 'modules/permissions/routes';
+import VehicleGroupsRoutes from 'modules/vehicle_groups/routes';
 
 const routes = {
   component: Layout,
